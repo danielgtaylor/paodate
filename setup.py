00129ea761b3c3203d3a1c74b41137bd136788ef
Daniel G. Taylor <danielgtaylor@gmail.com>
2012-06-15 20:03:39 -0700

    Update pypi info and make package available so that it can be installed easily with pip and easy_install

--- setup.py
+++ setup.py
@@ -3,7 +3,30 @@ from distutils.core import setup
 setup(
     name = "paodate",
     version = "1.2",
+    description = "Easy object-oriented date and time handling for Python",
+    long_description = """Overview
+========
+Utilities for making date and time handling in Python easy. This is mainly
+accomplished with the new Date object which abstracts most of the 
+differences between datetime, date, time, timedelta, and relativedelta,
+allowing you to convert freely between all of them and providing useful
+utility methods.
+
+Please look at the README.rst file included with this project and the
+source code documentation and examples for more information.
+""",
+    author = "Daniel G. Taylor",
+    author_email = "dan@programmer-art.org",
+    url = "https://github.com/danielgtaylor/paodate",
+    download_url = "https://github.com/danielgtaylor/paodate/zipball/master",
     py_modules = [
         "paodate",
+    ],
+    provides = [
+        "paodate",
+    ],
+    keywords = "date time timespan sql",
+    platforms = [
+        "Platform Independent",
     ],
 )
